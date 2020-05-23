0f346ff00dd351cc35532dbdff828332427040b0
George Lei <george@lei.nz>
2020-05-24 00:21:47 +1200

    change pypi package name

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="algorithm-training",
+    name="algorithm-prep",
     version="0.0.1",
     author="George Lei",
     author_email="george@lei.nz",
-    description="A training package for Python programmers to practice the algorithms",
+    description="Programming algorithm training package for programmers and students",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/georgezlei/algorithm-training-py",
